1d084a92d4a2b4e921e6eced8a9fd46966275add
Projjol Banerji <probaner23@gmail.com>
2014-11-06 22:31:48 +0530

    chp 5

--- tango/settings.py
+++ tango/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango',
 )
 
 MIDDLEWARE_CLASSES = (
--- tango/urls.py
+++ tango/urls.py
@@ -14,3 +14,11 @@ urlpatterns = patterns('',
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+	urlpatterns += patterns(
+		'django.views.static', 
+		(r'media/(?P<path>.*)', 
+		'serve',
+		{'document_root': settings.MEDIA_ROOT}),
+		) 
