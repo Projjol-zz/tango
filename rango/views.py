1744c1cc03b46a3f78009ec66de6712c3145389b
Projjol Banerji <probaner23@gmail.com>
2014-11-06 23:06:24 +0530

    index updated

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from rango.models import Category
 
 def index(request):
-	#return HttpResponse("Rango says hi")
 	
+	#Obtain context
 	context = RequestContext(request)
-	context_dic = {'boldmessage': "Bold font"}
+	#Arrange categories on the basis of likes
+	category_list = Category.objects.order_by('-id')[:5]
+	context_dict = {'categories':category_list}
 
-	return render_to_response('rango/index.html', context_dic,context)
+	#render the dictionary
+
+	return render_to_response('rango/index.html', context_dict,context)
